e29adce6db92972a82ddfc8925a1954b08a91956
Jeff Swisher <jefftswisher@gmail.com>
2019-12-17 19:08:49 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -51,11 +51,10 @@ class Student
   end 
   
   def self.new_from_db(row) 
-    student = self.new 
     student.id = row[0]
     student.name = row[1]
     student.grade = row[2]
-    student 
+    self.new = (student.id, student.name, student.grade) 
   end 
   
   def self.find_by_name(name)
