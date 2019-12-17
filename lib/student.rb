bfb13a96a8439da174165aa3a3df0345f501104c
Jeff Swisher <jefftswisher@gmail.com>
2019-12-17 18:47:57 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -47,9 +47,15 @@ class Student
     student 
   end 
   
-  def self.new_from_db 
-    
+  def self.new_from_db(row) 
+    student = Student.new 
+    student.id = row[0]
+    student.name = row[1]
+    student.grade = row[2]
+    student 
   end 
+  
+  
   
 
 
